7be7844bafe9deb95d644f1ca996507e6291c71d
qalle2 <qalle85@gmail.com>
2023-01-10 18:00:52 +0200

    verify with MD5

--- test_enc.sh
+++ test_enc.sh
@@ -12,22 +12,10 @@ python3 qromp_enc.py test-in-orig/smb2e.nes \
     test-in-patched/smb2e-fin.nes test-out/smb2e-fin.bps
 python3 qromp_enc.py test-in-orig/smb3e.nes \
     test-in-patched/smb3e-fin-bps.nes test-out/smb3e-fin.bps
-echo
+echo "Original:"
 ls -l test-in-patch/*.bps
-echo
+echo "Created:"
 ls -l test-out/*.bps
-echo
-
-echo "=== Verify BPS patches by applying them ==="
-python3 qromp.py test-in-orig/megaman4u.nes \
-    test-out/megaman4u-fin.bps test-out/megaman4u-fin.nes
-python3 qromp.py test-in-orig/smb2e.nes \
-    test-out/smb2e-fin.bps test-out/smb2e-fin.nes
-python3 qromp.py test-in-orig/smb3e.nes \
-    test-out/smb3e-fin.bps test-out/smb3e-fin-bps.nes
-diff test-in-patched/megaman4u-fin.nes test-out/megaman4u-fin.nes
-diff test-in-patched/smb2e-fin.nes     test-out/smb2e-fin.nes
-diff test-in-patched/smb3e-fin-bps.nes test-out/smb3e-fin-bps.nes
 echo
 
 echo "=== Create IPS patches ==="
@@ -37,22 +25,31 @@ python3 qromp_enc.py test-in-orig/megaman2u.nes \
     test-in-patched/megaman2u-fin.nes test-out/megaman2u-fin.ips
 python3 qromp_enc.py test-in-orig/smb3e.nes \
     test-in-patched/smb3e-fin-ips.nes test-out/smb3e-fin.ips
-echo
+echo "Original:"
 ls -l test-in-patch/*.ips
-echo
+echo "Created:"
 ls -l test-out/*.ips
 echo
 
-echo "=== Verify IPS patches by applying them ==="
+echo "=== Apply patches and verify patched files ==="
+# apply BPS
+python3 qromp.py test-in-orig/megaman4u.nes \
+    test-out/megaman4u-fin.bps test-out/megaman4u-fin.nes
+python3 qromp.py test-in-orig/smb2e.nes \
+    test-out/smb2e-fin.bps test-out/smb2e-fin.nes
+python3 qromp.py test-in-orig/smb3e.nes \
+    test-out/smb3e-fin.bps test-out/smb3e-fin-bps.nes
+# apply IPS
 python3 qromp.py test-in-orig/ducktales-e.nes \
     test-out/ducktales-e-fin.ips test-out/ducktales-e-fin.nes
 python3 qromp.py test-in-orig/megaman2u.nes \
     test-out/megaman2u-fin.ips test-out/megaman2u-fin.nes
 python3 qromp.py test-in-orig/smb3e.nes \
     test-out/smb3e-fin.ips test-out/smb3e-fin-ips.nes
-diff test-in-patched/ducktales-e-fin.nes test-out/ducktales-e-fin.nes
-diff test-in-patched/megaman2u-fin.nes   test-out/megaman2u-fin.nes
-diff test-in-patched/smb3e-fin-ips.nes   test-out/smb3e-fin-ips.nes
+# verify
+cd test-out
+md5sum -c --ignore-missing --quiet ../patched.md5
+cd ..
 echo
 
 echo "=== Four distinct errors ==="
